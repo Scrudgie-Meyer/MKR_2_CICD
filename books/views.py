bc750ce3389759f838d72e80fe6d2e8fcc0f336d
Waldemar Petrash <116195123+Scrudgie-Meyer@users.noreply.github.com>
2024-05-31 13:01:03 +0300

    Update views.py

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Author, Book
+from books import views
 
 def author_details(request, name):
     author = get_object_or_404(Author, name=name)
